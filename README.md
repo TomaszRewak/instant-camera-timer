b1aeb7827f37559c2e0e69cf1deb34742abcc13c
Tomasz Rewak <tomaszrewak@gmail.com>
2023-10-26 23:08:51 +0200

    Adding the delay logic to the program.

--- README.md
+++ README.md
@@ -3,14 +3,14 @@
 On Linux:
 
 ```bash
-avr-gcc main.c -o main.elf -mmcu=attiny13 -DF_CPU=1200000UL -Os
+avr-gcc main.c -o main.elf -mmcu=attiny13a -DF_CPU=1200000UL -Os
 avr-objcopy main.elf -O ihex main.hex
 ```
 
 On Windows:
 
 ```bash
-avrdude -c usbasp -p attiny13 -U flash:w:main.hex
+avrdude -c usbasp -p attiny13a -U flash:w:main.hex
 ```
 
 # Board
--- main.c
+++ main.c
@@ -6,13 +6,44 @@
 #define BUTTON_PIN PB1
 #define OUTPUT_PIN PB0
 
+#define SIGNAL_DURATION_MS 300
+#define SHORT_BUTTON_PRESS_TIME_MS 1500
+#define BUTTON_PROBING_INTERVAL_MS 100
+#define LONG_BUTTON_PRESS_SIGNAL_DELAY_MS 10000
+
 volatile uint8_t button_pressed = 0;
 
 void send_signal()
 {
     PORTB |= (1 << OUTPUT_PIN);
-    _delay_ms(1000);
+    _delay_ms(SIGNAL_DURATION_MS);
     PORTB &= ~(1 << OUTPUT_PIN);
+}
+
+short is_button_pressed()
+{
+    return PINB & (1 << BUTTON_PIN);
+}
+
+short is_long_button_press()
+{
+    int delay = 0;
+
+    do
+    {
+        _delay_ms(BUTTON_PROBING_INTERVAL_MS);
+        delay += BUTTON_PROBING_INTERVAL_MS;
+    } while (is_button_pressed() && delay < SHORT_BUTTON_PRESS_TIME_MS);
+
+    return delay >= SHORT_BUTTON_PRESS_TIME_MS;
+}
+
+short wait_for_button_release()
+{
+    while (is_button_pressed())
+    {
+        _delay_ms(BUTTON_PROBING_INTERVAL_MS);
+    }
 }
 
 ISR(INT0_vect)
@@ -31,10 +62,6 @@ int main()
     // Disable unused peripherals for power saving.
     ADCSRA &= ~(1 << ADEN); // Disable ADC.
 
-    // // Add PCINT1 interrupt.
-    // GIMSK |= (1 << PCIE);
-    // PCMSK |= (1 << PCINT1);
-
     // Enable the INT0 interrupt.
     GIMSK |= (1 << INT0);
     MCUCR |= (1 << ISC00) | (1 << ISC01);
@@ -45,7 +72,7 @@ int main()
     // Enable interrupts.
     sei();
 
-    // Set sleep mode to power down.
+    // Set sleep mode to idle.
     set_sleep_mode(SLEEP_MODE_IDLE);
 
     // Infinite loop.
@@ -55,8 +82,16 @@ int main()
 
         if (button_pressed)
         {
-            button_pressed = 0;
+            if (is_long_button_press())
+            {
+                wait_for_button_release();
+
+                _delay_ms(LONG_BUTTON_PRESS_SIGNAL_DELAY_MS);
+            }
+
             send_signal();
+
+            button_pressed = 0;
         }
     }
 }
