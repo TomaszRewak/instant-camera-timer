6cc8245c52c0ea7e4d67fdc2b4fbf43f7c2ae808
Tomasz Rewak <tomaszrewak@gmail.com>
2023-10-26 21:32:24 +0200

    Fighting with the sleep mode.

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Build and upload
 
+On Linux:
+
 ```bash
-avr-gcc -Os -DF_CPU=1200000UL -mmcu=attiny13 -c -o main.o main.c
+avr-gcc main.c -o main.elf -mmcu=attiny13 -DF_CPU=1200000UL -Os
+avr-objcopy main.elf -O ihex main.hex
 ```
+
+On Windows:
+
+```bash
+avrdude -c usbasp -p attiny13 -U flash:w:main.hex
+```
+
+# Board
+
+100kΩ
--- main.c
+++ main.c
@@ -3,31 +3,27 @@
 #include <util/delay.h>
 #include <avr/sleep.h>
 
-#define BUTTON_PIN 2
-#define OUTPUT_PIN 0
+#define BUTTON_PIN PB1
+#define OUTPUT_PIN PB0
 
 volatile uint8_t button_pressed = 0;
-
-ISR(INT0_vect)
-{
-    button_pressed = 1;
-}
 
 void send_signal()
 {
     PORTB |= (1 << OUTPUT_PIN);
-    _delay_ms(100);
+    _delay_ms(1000);
     PORTB &= ~(1 << OUTPUT_PIN);
+}
+
+ISR(INT0_vect)
+{
+    send_signal();
+    //button_pressed = 1;
 }
 
 int is_button_pressed()
 {
-    return !(PINB & (1 << BUTTON_PIN));
-}
-
-void wait_10_seconds()
-{
-    _delay_ms(10000);
+    return PINB & (1 << BUTTON_PIN);
 }
 
 int main()
@@ -35,9 +31,12 @@ int main()
     // Set output pin as output.
     DDRB |= (1 << OUTPUT_PIN);
 
+    // Set output pin low.
+    PORTB &= ~(1 << OUTPUT_PIN);
+
     // Enable the INT0 interrupt.
     GIMSK |= (1 << INT0);
-    MCUCR |= (1 << ISC01);
+    MCUCR |= (1 << ISC00) | (1 << ISC01);
 
     // Set the button pin as an input.
     DDRB &= ~(1 << BUTTON_PIN);
@@ -45,38 +44,44 @@ int main()
     // Enable interrupts.
     sei();
 
+    // Set sleep mode to power down.
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+
     // Infinite loop.
     while (1)
     {
-        // Wait for button press.
-        while (!button_pressed)
-        {
-            // Sleep until an interrupt occurs.
-            sleep_mode();
-        }
+        sleep_mode();
+        _delay_ms(2000);
 
-        // Wait for button release.
-        uint8_t sleep_count = 0;
-        do
-        {
-            _delay_ms(100);
-            sleep_count++;
-        } while (sleep_count < 10 && is_button_pressed());
+        // // Wait for button press.
+        // while (!button_pressed)
+        // {
+        //     // Sleep until an interrupt occurs.
+        //     sleep_mode();
+        // }
 
-        // Send the signal...
-        if (sleep_count < 10)
-        {
-            // ...immediately.
-            send_signal();
-        }
-        else
-        {
-            // ...after 10 seconds.
-            wait_10_seconds();
-            send_signal();
-        }
+        // // Wait for button release.
+        // uint8_t sleep_count = 0;
+        // do
+        // {
+        //     _delay_ms(100);
+        //     sleep_count++;
+        // } while (sleep_count < 10 && is_button_pressed());
 
-        // Clear the button pressed flag.
-        button_pressed = 0;
+        // // Send the signal...
+        // if (sleep_count < 10)
+        // {
+        //     // ...immediately.
+        //     send_signal();
+        // }
+        // else
+        // {
+        //     // ...after 5 seconds.
+        //     _delay_ms(5000);
+        //     send_signal();
+        // }
+
+        // // Clear the button pressed flag.
+        // button_pressed = 0;
     }
 }
